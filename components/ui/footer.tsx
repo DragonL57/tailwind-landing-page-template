5b2184b9e75a53a89bd1ed944413da71cdb0212c
DragonL57 <vmthelong2004@gmail.com>
2026-02-12 17:19:44 +0700

    Update Footer and Header components for improved layout; adjust Logo size for better visibility

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -11,7 +11,7 @@ export default function Footer({ border = false }: { border?: boolean }) {
         >
           {/* 1st block */}
           <div className="space-y-2 sm:col-span-12 lg:col-span-4">
-            <div>
+            <div className="scale-125">
               <Logo />
             </div>
             <div className="text-sm text-gray-600">
@@ -75,7 +75,7 @@ export default function Footer({ border = false }: { border?: boolean }) {
                   className="text-gray-600 transition hover:text-vmg-blue"
                   href="#0"
                 >
-                  Tầm Nhìn & Sứ Mạng
+                  Tầm Nhìn & Sứ 
                 </Link>
               </li>
               <li>
